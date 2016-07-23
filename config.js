0dd234062f67be6d70bb47880b9c98789a6a6e9d
virenb <virenb@gmail.com>
2016-07-23 17:01:20 +0000

    Updated config with mlab config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://<virenb>:<hakeem3>@ds027175.mlab.com:27175/mongo-shopping-list' :
-                            'mongodb://<virenb>:<hakeem3>@ds027175.mlab.com:27175/mongo-shopping-list');
+                            'mongodb://virenb:hakeem3@ds027175.mlab.com:27175/mongo-shopping-list' :
+                            'mongodb://virenb:hakeem3@ds027175.mlab.com:27175/mongo-shopping-list');
 exports.PORT = process.env.PORT || 8080;
